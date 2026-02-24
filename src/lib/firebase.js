e24f7f99470f64b1878e4e907379fe9f34ce17d7
pradeep-jais <pradeepjais13@gmail.com>
2026-02-24 20:02:21 +0530

    feat: firebase integrated, app and global authentication object initialized

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,1 +1,16 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3p8jWuBs54B5ftBYQjdAjecGrwt5kyYM",
+  authDomain: "project-manager-lite.firebaseapp.com",
+  projectId: "project-manager-lite",
+  storageBucket: "project-manager-lite.firebasestorage.app",
+  messagingSenderId: "263893887974",
+  appId: "1:263893887974:web:800ba6c80d0dc0a3120427",
+  measurementId: "G-2H0X4M4TTL",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
