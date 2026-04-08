f9a7a5083457baea0a7b5b97f90f82160c9a961d
pradeep-jais <pradeepjais13@gmail.com>
2026-04-08 19:47:28 +0530

    feat: static UI for project page and add task form created

---
+++ src/pages/Dashboard/AddTask.jsx
@@ -0,0 +1,89 @@
+import Modal from "../../components/Modal";
+
+import { useState } from "react";
+
+const AddTask = ({ setIsModalOpen }) => {
+  const [task, setTask] = useState({
+    name: "",
+    deadline: "",
+    status: "pending",
+  });
+  const handleTaskInput = (e) => {
+    setTask((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+  const handleTaskSubmit = (e) => {
+    e.preventDefault();
+    console.log("Task submitted:", task);
+
+    setTask({
+      name: "",
+      deadline: "",
+      status: "pending",
+    });
+    setIsModalOpen(false);
+  };
+  return (
+    <Modal>
+      <form
+        onSubmit={handleTaskSubmit}
+        className={`w-full bg-surface flex flex-col gap-4 p-8 max-w-lg rounded-sm shadow-md relative`}
+      >
+        <button
+          className="absolute top-4 right-5 text-red-500 text-2xl font-bold cursor-pointer"
+          type="button"
+          onClick={() => setIsModalOpen(false)}
+        >
+          X
+        </button>
+        <h3 className="capitalize text-lg font-bold">Add New Tasks</h3>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="taskName">Task name</label>
+          <input
+            type="text"
+            id="taskName"
+            name="name"
+            value={task.name}
+            onChange={handleTaskInput}
+            className="border border-border px-2 py-1 rounded-sm focus:outline-none focus:ring-1 focus:ring-primary"
+            placeholder="New task"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="taskDeadline">Deadline</label>
+          <input
+            type="date"
+            id="taskDeadline"
+            name="deadline"
+            value={task.deadline}
+            onChange={handleTaskInput}
+            className="border border-border px-2 py-1 rounded-sm focus:outline-none focus:ring-1 focus:ring-primary"
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="taskStatus">Status</label>
+          <select
+            name="status"
+            id="taskStatus"
+            value={task.status}
+            onChange={handleTaskInput}
+          >
+            <option value="pending">pending</option>
+            <option value="in progress">in progress</option>
+            <option value="completed">completed</option>
+          </select>
+        </div>
+        <div className="flex justify-end mt-4">
+          <button
+            className="bg-primary hover:bg-primaryHover transform duration-300 text-white text-sm py-1 px-4 capitalize rounded-md cursor-pointer flex items-center gap-2"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </Modal>
+  );
+};
+export default AddTask;
--- src/pages/Dashboard/Project.jsx
+++ src/pages/Dashboard/Project.jsx
@@ -1,4 +1,67 @@
+import { useState } from "react";
+import AddTask from "./AddTask";
+import Modal from "../../components/Modal";
+
 const Project = () => {
-  return <div>Project</div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <section className="max-w-7xl mx-auto p-2">
+      <p className="mb-2 bg-surface p-2 rounded-sm shadow-3xl">
+        Welcome, Pradeep !
+      </p>
+      <div className="flex items-center gap-2 mb-4">
+        <label>Your Project:</label>
+        <h2 className="text-xl font-medium">Build a portfolio app</h2>
+      </div>
+      <div className="mb-2 bg-surface p-2 rounded-sm shadow-sm">
+        <div className="flex justify-between border-b border-border mb-2 pb-2">
+          <h3 className="text-lg font-semibold">Tasks</h3>
+          <button
+            className="bg-primary hover:bg-primaryHover text-white text-sm  py-1 px-3 rounded-md cursor-pointer"
+            onClick={() => setIsModalOpen(true)}
+          >
+            Add Task
+          </button>
+        </div>
+        {isModalOpen && <AddTask setIsModalOpen={setIsModalOpen} />}
+        <table className="w-full mb-2">
+          <thead className="bg-background font-bold">
+            <tr>
+              <td>Your Task</td>
+              <td>Deadline</td>
+              <td>Status</td>
+              <td>Actions</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Create folder structure</td>
+              <td>2023-10-31</td>
+              <td>In progress</td>
+              <td>
+                <button className="mr-2 cursor-pointer">📝</button>
+                <button className="cursor-pointer">❌</button>
+              </td>
+            </tr>
+          </tbody>
+          <tbody className="bg-background">
+            <tr>
+              <td>Initialize firebase</td>
+              <td>2023-10-31</td>
+              <td>Pending</td>
+              <td>
+                <button className="mr-2 cursor-pointer">📝</button>
+                <button className="cursor-pointer">❌</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <button className="bg-primary hover:bg-primaryHover text-white text-sm  py-1 px-3 rounded-md cursor-pointer">
+          Clear Tasks
+        </button>
+      </div>
+    </section>
+  );
 };
 export default Project;
