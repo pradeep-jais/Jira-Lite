a305dc4d225df783d62dd0dd6647057548e5f325
pradeep-jais <pradeepjais13@gmail.com>
2026-04-09 21:24:51 +0530

    ui: project page UI transformed into professional polished app with proper visual hierarchy, font, spacing, CTA, icons, beautifull tables

--- src/pages/Dashboard/Project.jsx
+++ src/pages/Dashboard/Project.jsx
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react";
 import AddTask from "./AddTask";
+import {
+  Plus,
+  Trash2,
+  SquarePen,
+  EllipsisVertical,
+  CircleDashed,
+} from "lucide-react";
 
 import {
   collection,
@@ -85,18 +92,32 @@ const Project = () => {
     }
   };
 
+  const pendingTasksCount = tasks.filter(
+    (task) => task.status === "pending",
+  ).length;
+
+  const statusPillColor = (status) => {
+    let colorClass = "";
+    if (status === "pending") colorClass = "bg-gray-200 text-textPrimary";
+    if (status === "completed") colorClass = "bg-success text-white";
+    if (status === "in progress") colorClass = "bg-blue-500 text-white";
+    return colorClass;
+  };
+
   return (
-    <section className="max-w-7xl mx-auto p-2">
-      <p className="mb-2 bg-surface p-2 rounded-sm shadow-3xl">
-        Welcome, Pradeep !
-      </p>
-      <div className="flex items-center gap-2 mb-4">
-        <label>Your Project:</label>
-        <h2 className="text-xl font-medium">Build a portfolio app</h2>
-      </div>
-      <div className="mb-2 bg-surface p-2 rounded-sm shadow-sm">
-        <div className="flex justify-between border-b border-border mb-2 pb-2">
-          <h3 className="text-lg font-semibold">Tasks</h3>
+    <section className="max-w-5xl mx-auto p-2">
+      <div className="mb-2 bg-surface p-2 rounded-md shadow-sm">
+        <div className="flex justify-between items-center">
+          <div>
+            <h2 className="text-xl font-medium text-textPrimary">
+              Build a portfolio app
+            </h2>
+            <div className="flex gap-2 text-textSecondary px-1">
+              <span>{tasks.length} Task</span>
+              <span className="w-1.5 h-1.5 rounded-full bg-gray-700 self-end mb-1.5"></span>
+              <span>{pendingTasksCount} Pending</span>
+            </div>
+          </div>
           <button
             className="bg-primary hover:bg-primaryHover text-white text-sm  py-1 px-3 rounded-md cursor-pointer"
             onClick={() => {
@@ -107,6 +128,16 @@ const Project = () => {
             Add Task
           </button>
         </div>
+      </div>
+
+      <div className="mb-2 bg-surface p-2 rounded-sm shadow-sm">
+        <div className="flex justify-between border-b border-border mb-2 pb-2">
+          <h3 className="text-lg font-semibold">Tasks</h3>
+          <button className="cursor-pointer hover:text-primary transition">
+            <EllipsisVertical />
+          </button>
+        </div>
+
         {isModalOpen && (
           <AddTask
             tasks={tasks}
@@ -120,51 +151,68 @@ const Project = () => {
         {tasks.length === 0 ? (
           <p>No tasks available.</p>
         ) : (
-          <table className="w-full mb-2">
-            <thead className="bg-background font-bold">
-              <tr>
-                <td>Name</td>
-                <td>Deadline</td>
-                <td>Status</td>
-                <td>Actions</td>
-              </tr>
-            </thead>
-            {tasks.map((task, i) => {
-              const { id, name, deadline, status } = task;
-              return (
-                <tbody key={i}>
-                  <tr>
-                    <td>{name}</td>
-                    <td>{deadline}</td>
-                    <td>{status}</td>
-                    <td>
-                      <button
-                        className="mr-2 cursor-pointer"
-                        onClick={() => {
-                          setIsModalOpen(true);
-                          setAction({ name: "edit", id });
-                        }}
-                      >
-                        📝
-                      </button>
-                      <button
-                        className="cursor-pointer"
-                        onClick={() => removeTask(id)}
-                      >
-                        ❌
-                      </button>
-                    </td>
-                  </tr>
-                </tbody>
-              );
-            })}
-          </table>
-        )}
-
-        {tasks.length > 0 && (
-          <button className="bg-primary hover:bg-primaryHover text-white text-sm  py-1 px-3 rounded-md cursor-pointer">
-            Clear Tasks
-          </button>
+          <div className="overflow-x-auto">
+            <table className="w-full mb-2 text-sm min-w-max">
+              <thead>
+                <tr className="bg-background font-semibold text-left text-textPrimary">
+                  <th className="p-2">Name</th>
+                  <th className="p-2">Deadline</th>
+                  <th className="p-2">Status</th>
+                  <th className="p-2">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="p-2">
+                    <button
+                      className="bg-gray-100 text-sm py-1 px-3 rounded-lg text-primary font-bold hover:bg-gray-200 transition cursor-pointer flex items-center gap-1"
+                      onClick={() => {
+                        setIsModalOpen(true);
+                        setAction({ name: "add", id: "" });
+                      }}
+                    >
+                      <Plus size={20} /> Add Task
+                    </button>
+                  </td>
+                </tr>
+                {tasks.map((task, i) => {
+                  const { id, name, deadline, status } = task;
+                  return (
+                    <tr key={id} className="text-textPrimary">
+                      <td className="p-2 flex items-center gap-2">
+                        <CircleDashed size={20} /> {name}
+                      </td>
+                      <td className="p-1">{deadline}</td>
+                      <td className="p-1">
+                        <span
+                          className={`w-25 flex justify-center items-center ${statusPillColor(status)} capitalize py-0.5 px-2 rounded-2xl`}
+                        >
+                          {status}
+                        </span>
+                      </td>
+                      <td className="p-1">
+                        <button
+                          className="mr-2 cursor-pointer hover:text-primary"
+                          onClick={() => {
+                            setIsModalOpen(true);
+                            setAction({ name: "edit", id });
+                          }}
+                        >
+                          <SquarePen size={20} />
+                        </button>
+                        <button
+                          className="cursor-pointer hover:text-primary transition"
+                          onClick={() => removeTask(id)}
+                        >
+                          <Trash2 size={20} />
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
         )}
       </div>
     </section>
